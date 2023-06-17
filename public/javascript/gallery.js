970913b5426a5d4e2be00cb445f0a2f36304c54f
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-06-17 17:45:06 +0200

    working on gallery

--- public/javascript/gallery.js
+++ public/javascript/gallery.js
@@ -1,20 +1,39 @@
+/* to do: loadItems function
+const galleryObserver = new IntersectionObserver((entries)=>{
+    if (entries[0].intersectionRatio <= 0) return;
+    loadItems(10);
+    console.log('Loaded new imgs')
+}) 
+galleryObserver.observe(document.querySelector('.gallery'))*/
+// zjišťování šířky a výšky img a vytvoření classy k div
 /* document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.gallery_item');
-
-    for (let i = 0; i < images.length; i++) {
-        var image = images[i];
-
-        // Checking image width/height
-        if (image.naturalHeight < image.naturalWidth) {
-            image.classList.add('horizontalImg');
+    const imgList = document.querySelectorAll('.gallery_item');
+  
+    imgList.forEach((img) => {
+      const dynamicImg = new Image();
+  
+      dynamicImg.src = img.src;
+  
+      dynamicImg.onload = function() {
+        const width = this.width;
+        const height = this.height;
+        const galleryCol = img.closest('.gallery_col');
+  
+        if (width < height) {
+          galleryCol.classList.add('horizontalImg');
+        } else if (width === height) {
+          galleryCol.classList.add('squareImg');
         } else {
-            image.classList.add('verticalImg');
+          galleryCol.classList.add('verticalImg');
         }
-    } 
-});*/
-    const gallery = document.querySelector('.gallery');
-    const masonry = new Masonry(gallery, {
-        itemSelector: '.gallery_col',
-        gutter:10,
+      };
     });
-
+  }); */
+  
+  const gallery = document.querySelector('.gallery');
+  const masonry = new Masonry(gallery, {
+    itemSelector: '.gallery_col',
+    gutter: 10,
+    percentPosition: true
+  });
+  
