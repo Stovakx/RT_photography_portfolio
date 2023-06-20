c42f00f2eec44cce05dd4030ed7f4c1a8378f113
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-06-20 19:56:38 +0200

    work on gallery

--- public/javascript/gallery.js
+++ public/javascript/gallery.js
@@ -1,39 +1,18 @@
-/* to do: loadItems function
-const galleryObserver = new IntersectionObserver((entries)=>{
-    if (entries[0].intersectionRatio <= 0) return;
-    loadItems(10);
-    console.log('Loaded new imgs')
-}) 
-galleryObserver.observe(document.querySelector('.gallery'))*/
-// zjišťování šířky a výšky img a vytvoření classy k div
-/* document.addEventListener('DOMContentLoaded', () => {
-    const imgList = document.querySelectorAll('.gallery_item');
-  
-    imgList.forEach((img) => {
-      const dynamicImg = new Image();
-  
-      dynamicImg.src = img.src;
-  
-      dynamicImg.onload = function() {
-        const width = this.width;
-        const height = this.height;
-        const galleryCol = img.closest('.gallery_col');
-  
-        if (width < height) {
-          galleryCol.classList.add('horizontalImg');
-        } else if (width === height) {
-          galleryCol.classList.add('squareImg');
-        } else {
-          galleryCol.classList.add('verticalImg');
-        }
-      };
-    });
-  }); */
-  
+window.onload = () => {
+  const galleryCols = document.querySelectorAll('.gallery_col');
+  const marginSize = 10;
+
+  galleryCols.forEach((col) => {
+    const img = col.querySelector('.gallery_item img');
+    const colWidth = col.offsetWidth;
+    const imgAspectRatio = img.naturalWidth / img.naturalHeight;
+    const colHeight = colWidth / imgAspectRatio;
+    col.style.height = colHeight + 'px';
+  });
+
   const gallery = document.querySelector('.gallery');
   const masonry = new Masonry(gallery, {
     itemSelector: '.gallery_col',
-    gutter: 10,
-    percentPosition: true
+    gutter: marginSize,
   });
-  
+};
